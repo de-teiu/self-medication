482d2ce5d9d6917dd3e39cfdedbb37d22c4d0b45
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2018-01-02 15:52:43 +0900

    フッター作成中

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+// register modal component
+Vue.component('modal', {
+    template: '#modal-template'
+})
+
+// start app
+new Vue({
+    el: '#app',
+    data: {
+        showModal: false
+    }
+})
